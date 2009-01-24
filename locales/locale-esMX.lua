a790f66f6c99368ed559d912c1d788bb778ddbe8
John Pasula <ackis@shaw.ca>
2009-01-24 06:57:08 +0000

    Add missing locales for the app, fix my type with enUS.

---
+++ locales/locale-esMX.lua
@@ -0,0 +1,5 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("IHML", "esMX")
+
+if not L then return end
+
+--@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=false, same-key-is-true=true)@
